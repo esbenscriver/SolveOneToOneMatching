e91511b71f1e7edb3c53ef0b32b015a71df2587f
esbenscriver <esbenscriver@gmail.com>
2025-09-16 13:46:08 +0200

    loop over fixed-point solvers

--- examples/main.py
+++ examples/main.py
@@ -33,11 +33,6 @@ from SolveOneToOneMatching.DiscreteChoiceModel import (
 # Increase precision to 64 bit
 jax.config.update("jax_enable_x64", True)
 
-# choose accelerator of fixed-point iterations
-fixed_point_solver = FixedPointIteration
-fixed_point_solver = AndersonAcceleration
-fixed_point_solver = SquaremAcceleration
-
 # Set number of types of agents on both sides of the market
 types_X, types_Y = 4, 6
 
@@ -76,56 +71,57 @@ nest_share_Y = random.dirichlet(
     key=random.PRNGKey(312), alpha=jnp.ones((nests_X,)), shape=(types_X,)
 )
 
-print("-----------------------------------------------------------------------")
-print("1. Solve a matching model with logit demand:")
+for fixed_point_solver in [FixedPointIteration, AndersonAcceleration, SquaremAcceleration]:
+    print("-----------------------------------------------------------------------")
+    print("1. Solve a matching model with logit demand:")
 
-model_logit = MatchingModel(
-    model_X=LogitModel(utility=utility_X, scale=scale_X, n=n_X),
-    model_Y=LogitModel(utility=utility_Y, scale=scale_Y, n=n_Y),
-)
+    model_logit = MatchingModel(
+        model_X=LogitModel(utility=utility_X, scale=scale_X, n=n_X),
+        model_Y=LogitModel(utility=utility_Y, scale=scale_Y, n=n_Y),
+    )
 
-solution_logit = model_logit.Solve(fixed_point_solver=fixed_point_solver)
+    solution_logit = model_logit.Solve(fixed_point_solver=fixed_point_solver)
 
-print("-----------------------------------------------------------------------")
-print("2. Solve a matching model with nested logit demand:")
+    print("-----------------------------------------------------------------------")
+    print("2. Solve a matching model with nested logit demand:")
 
-model_nested_logit = MatchingModel(
-    model_X=NestedLogitModel(
-        utility=utility_X,
-        scale=scale_X,
-        nest_index=nest_index_X,
-        nest_parameter=nest_parameter_X,
-        n=n_X,
-    ),
-    model_Y=NestedLogitModel(
-        utility=utility_Y,
-        scale=scale_Y,
-        nest_index=nest_index_Y,
-        nest_parameter=nest_parameter_Y,
-        n=n_Y,
-    ),
-)
+    model_nested_logit = MatchingModel(
+        model_X=NestedLogitModel(
+            utility=utility_X,
+            scale=scale_X,
+            nest_index=nest_index_X,
+            nest_parameter=nest_parameter_X,
+            n=n_X,
+        ),
+        model_Y=NestedLogitModel(
+            utility=utility_Y,
+            scale=scale_Y,
+            nest_index=nest_index_Y,
+            nest_parameter=nest_parameter_Y,
+            n=n_Y,
+        ),
+    )
 
-solution_nested_logit = model_nested_logit.Solve(fixed_point_solver=fixed_point_solver)
+    solution_nested_logit = model_nested_logit.Solve(fixed_point_solver=fixed_point_solver)
 
-print("-----------------------------------------------------------------------")
-print("3. Solve a matching model with generalized nested logit demand:")
+    print("-----------------------------------------------------------------------")
+    print("3. Solve a matching model with generalized nested logit demand:")
 
-model_GNL = MatchingModel(
-    model_X=GeneralizedNestedLogitModel(
-        utility=utility_X,
-        scale=scale_X,
-        nest_share=nest_share_X,
-        nest_parameter=nest_parameter_X,
-        n=n_X,
-    ),
-    model_Y=GeneralizedNestedLogitModel(
-        utility=utility_Y,
-        scale=scale_Y,
-        nest_share=nest_share_Y,
-        nest_parameter=nest_parameter_Y,
-        n=n_Y,
-    ),
-)
+    model_GNL = MatchingModel(
+        model_X=GeneralizedNestedLogitModel(
+            utility=utility_X,
+            scale=scale_X,
+            nest_share=nest_share_X,
+            nest_parameter=nest_parameter_X,
+            n=n_X,
+        ),
+        model_Y=GeneralizedNestedLogitModel(
+            utility=utility_Y,
+            scale=scale_Y,
+            nest_share=nest_share_Y,
+            nest_parameter=nest_parameter_Y,
+            n=n_Y,
+        ),
+    )
 
-solution_GNL = model_GNL.Solve(fixed_point_solver=fixed_point_solver)
+    solution_GNL = model_GNL.Solve(fixed_point_solver=fixed_point_solver)
